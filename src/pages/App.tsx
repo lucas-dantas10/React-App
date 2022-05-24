2369de6ed2838ccb6d5c02816b6fe8c8bdb63385
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-24 15:13:16 -0300

    adicionando o cronometro

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista';
 import style from './app.module.scss';
@@ -8,6 +8,7 @@ function App() {
     <div className={style.AppStyle}>
       <Formulario />
       <Lista/>
+      <Cronometro/>
     </div>
   );
 }
