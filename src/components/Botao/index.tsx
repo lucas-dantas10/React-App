267458bfad10db22e1df81618fbe9760b604be92
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-25 14:54:45 -0300

    adicionando o type no botao

--- src/components/Botao/index.tsx
+++ src/components/Botao/index.tsx
@@ -2,10 +2,17 @@ import React from 'react';
 import style from "./Botao.module.scss";
 
 export default class Botao extends React.Component<any, any> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            value: null,
+        }
+    }
     render() {
+        const { type="button" } = this.props;
         return (
-            <button className={style.botao}>
-                {this.props.text}
+            <button type={type} className={style.botao}>
+                {this.props.children}
             </button>
         );
     }
