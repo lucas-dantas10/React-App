40a625cce9aefec2c59b804677f97c212e575497
lucas-dantas10 <lucas.dantas.nogueira@gmail.com>
2022-05-26 15:27:01 -0300

    criando interface Props com os valores tarefas de ITarefas e selecionaTarefa, para pegar a tarefa selecionada

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -7,11 +7,23 @@ import style from './app.module.scss';
 
 function App() {
   const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionado: ITarefa) {
+    setSelecionado(tarefaSelecionado);
+    setTarefas(tarefasAnteriores => tarefasAnteriores.map(tarefa => ({
+      ...tarefa, 
+      selecionado: tarefa.id === tarefaSelecionado.id ? true : false
+    })))
+  }
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas} />
-      <Cronometro/>
+      <Lista 
+        tarefas={tarefas}
+        selecionaTarefa={selecionaTarefa}
+      />
+      <Cronometro selecionado={selecionado} />
     </div>
   );
 }
